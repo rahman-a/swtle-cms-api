a87863d83d8830251f18830a832ebe8ae99b9b07
rahman-a <ahm.abdrahman@gamil.com>
2023-10-12 10:41:25 +0300

    create single page for privacy policy and terms and conditions

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,5 +3,15 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_KlXkCGozmj7NoTDXBbX5Oens64hB/sII14Pdpsz",
+      apiToken: "MRH18eKEgYxndIG0X5ywc8W8",
+      appFilter: "swtle-cms-ui",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
   // ...
 });
